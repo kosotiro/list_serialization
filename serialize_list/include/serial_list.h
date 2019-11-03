eff3d0bba47d00dfe5ec2565279586047fafc70d
Constantinos Sotiropoulos <kosotiro@yahoo.gr>
2019-11-03 18:54:06 +0200

    Remove unecessary API functions

--- serialize_list/include/serial_list.h
+++ serialize_list/include/serial_list.h
@@ -9,11 +9,4 @@ void serialize_list_node_t(list_node_t *obj, ser_buff_t *b);
 list_t *de_serialize_list_t(ser_buff_t *b);
 list_node_t *de_serialize_list_node_t(ser_buff_t *b);
 
-/*print functions*/
-void print_list_t(list_t *obj);
-void print_list_node_t(list_node_t *obj);
-
-/*compute sum of integers */
-int sum_list(list_t *obj);
-
 #endif
